8d90d390c070f1037e148c240c906fae215439f6
rzkmak <rizkimaulanaakbar@outlook.com>
2021-03-27 20:39:11 +0700

    add: entrypoint library

--- src/types/resource/ResourceType.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ResourceType = void 0;
-var ResourceType;
-(function (ResourceType) {
-    ResourceType["Anime"] = "anime";
-    ResourceType["Manga"] = "manga";
-})(ResourceType = exports.ResourceType || (exports.ResourceType = {}));
